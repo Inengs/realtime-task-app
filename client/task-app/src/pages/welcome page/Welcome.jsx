da37f94fe9fe508b69fbe6247ffad7ac32bc5d76
Inengiye Emmanuel <inengiyeemmanuel@gmail.com>
2025-09-09 19:02:52 +0100

    landing page basically done

---
+++ client/task-app/src/pages/welcome page/Welcome.jsx
@@ -0,0 +1,33 @@
+import { ReactComponent as LeafSvg } from "../../assets/images/logo 2.svg"
+
+function IntroHeader() {
+    return (
+        <h1 className="text-3xl text-gray-800 font-bold">TaskFlow</h1>
+    )
+}
+
+const LeafIcon = ({ size = 100, color = "green" }) => (
+    <LeafSvg
+        width={size}
+        height={size}
+        fill={color}
+        className="mb-4 animate-bounce"
+    />
+);
+
+
+function IntroText() {
+    return (
+        <span className="text-lg text-gray-400 font-medium">Manage tasks Effortlessly</span>
+    )
+}
+
+export default function MyApp() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen space-y-3">
+            <LeafIcon size={80} color="green" />
+            <IntroHeader />
+            <IntroText />
+        </div>
+    )
+}
